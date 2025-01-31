8ec9bde2507193464ae46ff76e75dbab6584f258
Lunjie Li <lunjielee@qq.com>
2025-01-31 16:32:03 -0700

    Add Booking and MenuItem models to restaurant app

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255, null=True)
+    guest_number = models.SmallIntegerField(default=1)
+    date = models.DateField(null=True)
+    comment = models.CharField(max_length=1000, null=True)
+
+    def __str__(self):
+        return f'{self.name} : {str(self.date)}'
+
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    featured = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.title} : {str(self.price)}'
