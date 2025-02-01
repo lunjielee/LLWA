509b425a5b307089bd373ef2d76380bc52933ed9
Lunjie Li <lunjielee@qq.com>
2025-01-31 17:06:05 -0700

    Add BookingForm and Menu model to handle booking data and menu items

--- restaurant/models.py
+++ restaurant/models.py
@@ -20,3 +20,12 @@ class MenuItem(models.Model):
 
     def __str__(self):
         return f'{self.title} : {str(self.price)}'
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.IntegerField(null=False)
+    menu_item_description = models.TextField(max_length=1000, default='')
+
+    def __str__(self):
+        return self.name
