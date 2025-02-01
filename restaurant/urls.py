67ffee8e73b28d8943afe932328e0b538ec3ea9c
Lunjie Li <lunjielee@qq.com>
2025-01-31 17:06:24 -0700

    Update URL routing and add views for home, about, reservations, booking, menu, and menu item

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,7 +2,13 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-
+    path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('book/', views.book, name="book"),
+    path('reservations/', views.reservations, name="reservations"),
+    path('menu/', views.menu, name="menu"),
+    path('menu_item/<int:pk>/', views.display_menu_item, name="menu_item"),
+    path('bookings', views.bookings, name='bookings'),
     path('', views.index, name='index'),
 
     path('api/menu/', views.MenuItemsView.as_view()),
